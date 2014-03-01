c6ab24fd5167bea9fb01e7e8f2dfc82dcf44ec0e
Risa B <rbatta@gmail.com>
2014-03-01 12:07:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MavenYouth::Application.config.secret_key_base = 'cbd14feffe5cd83b5b9a775298eea162c0186ad288712d0e5bdeb93be4b946955c0b1d3b80006bd8d5d91f96f767b66d7070db0d7f593a240734f45916e99d45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MavenYouth::Application.config.session_store :cookie_store, key: '_maven_youth_session'
